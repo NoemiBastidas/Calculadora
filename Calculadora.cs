bad5522f523a8613c661ef915e961453c2f5d9f8
Noemí Bastidas <36025192+NoemiBastidas@users.noreply.github.com>
2018-06-04 08:31:21 -0500

    Create Calculadora.cs

---
+++ Calculadora.cs
@@ -0,0 +1,47 @@
+using System;
+namespace Calculadora
+{
+        
+        class Calculadora
+    {
+        public void sumar()
+        {
+            suma()
+        }
+        public void restar()
+        {
+            resta()
+        }
+        public void multiplicar()
+        {
+            multiplicacion()
+        }
+        public void dividir()
+        {
+            division()      
+    }
+            var Calculadora = new Calculadora();
+            var resultado=Calculadora.sumar(20,10);
+            public double suma(double numero1, double numero2);
+            console.writeline("Esto  suma");
+
+            var Calculadora= new Calculadora();
+            var resultado=Calculadora.restar(20,10);
+            public double resta (double numero1, double numero2);
+            console.writeline("Esto resta");
+
+            var Calculadora= new Calculadora();
+            var resultado=Calculadora.multiplicar(20,10);
+            public double multiplicacion (double numero1, double numero2);
+            console.writeline("Esto  multiplica");
+
+            var Calculadora= new Calculadora();
+            var resultado=Calculadora.dividir(20,10);
+            public double division (double numero1, double numero2);
+            console.writeline("Esto  dividi");
+
+            var Calculadora= new Calculadora();
+            var resultado=Calculadora.raices(20,10);
+            public double raiz (double numero1, double numero2);
+            console.writeline("Esto sirve para sacar la raiz");
+}
