a1d69dce48487180502a942d3b6de3b48abaeb51
Noemí Bastidas <36025192+NoemiBastidas@users.noreply.github.com>
2018-07-22 21:06:46 -0500

    Add files via upload

---
+++ conexxionBD.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace coneccionBD
+public class Estudiante 
+{
+    {
+        public int id;
+        public string nombre;
+        double int nota;
+        }
+public class AdministradorBdd         
+        {
+            public int Grabar(Estudianteporgrabar)
+            {
+                var con = new BaseDato();
+                return conectar(inset into estudiante(nombre,nota)values)
+            }
+            
+    }
+{
+
+ 
