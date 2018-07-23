20d9721eb10eacf5619da3744de16594d5d6cb84
Noemí Bastidas <36025192+NoemiBastidas@users.noreply.github.com>
2018-07-22 20:41:19 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,62 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace BurbujaOrdenamiento
+namespace OrdenamientoBurbuja
 {
-        class Burbuja
+    class Program
+    {
+        static void Main(string[] args)
         {
-            private int[] vector;
+            // Arreglo de ejemplo con 10 valores
+            int[] Valores = new int[10] { 10, 5, 35, 11, 3, 9, 15, 2, 12, 1 };
+            int Auxiliar = 0;
 
-            public void Ejecutar()
+            // Se hace un recorrido desde el segundo elemento hasta el ultimo
+            for (int i = 1; i < Valores.Length; i++)
             {
-                Console.WriteLine("Burbuja");
-                Console.Write("longitud del vector ");
-                string linea;
-                linea = Console.ReadLine();
-                int cant;
-                cant = int.Parse(linea);
-                vector = new int[cant];
-                for (int f = 0; f < vector.Length; f++)
+                for (int j = 0; j < Valores.Length - i; j++)
                 {
-                    Console.Write("Ingrese elemento " + (f + 1) + ": ");
-                    linea = Console.ReadLine();
-                    vector[f] = int.Parse(linea);
-                }
-            }
 
-            public void MetodoBurbuja()
-            {
-                int j;
-                for (int a = 1; a < vector.Length; a++)
-                    for (int b = vector.Length - 1; b >= a; b--)
+
+                    if (Valores[j] > Valores[j + 1])
                     {
-                        if (vector[b - 1] > vector[b])
-                        {
-                            j = vector[b - 1];
-                            vector[b - 1] = vector[b];
-                            vector[b] = j;
-                        }
+                        Auxiliar = Valores[j];
+                        Valores[j] = Valores[j + 1];
+                        Valores[j + 1] = Auxiliar;
                     }
-            }
-
-            public void Imprimir()
-            {
-                Console.WriteLine("Vector ordenados en forma ascendente");
-                for (int f = 0; f < vector.Length; f++)
-                {
-                    Console.Write(vector[f] + "  ");
                 }
-                Console.ReadKey();
             }
 
-            static void Main(string[] args)
+            for (int i = 0; i < Valores.Length; i++)
             {
-                Burbuja ordenable = new Burbuja();
-                ordenable.Ejecutar();
-                ordenable.MetodoBurbuja();
-                ordenable.Imprimir();
+                Console.WriteLine(Valores[i]);
             }
-        }    
+            Console.ReadLine();
+
+        }
+    }
+    
 }
